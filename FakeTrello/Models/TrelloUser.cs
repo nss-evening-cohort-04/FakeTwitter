17a9ffade9d82f2a6279101d399c001c3338c0d1
jessica kilby <jessicakilby@gmail.com>
2017-02-28 21:20:17 -0600

    added additional classes to models

---
+++ FakeTrello/Models/TrelloUser.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+//Entity is our Object Relational Mapper (ORM) So it knows how to interpret this information
+
+namespace FakeTrello.Models
+{
+    public class TrelloUser
+    {
+        [Key] //help tell db system what constraints to take, applies to first property it hits going down
+        public int TrelloUserId { get; set; } //Primary Key
+
+        //stacking of properties applies multiple annotations
+        //to the following property. failed = database failed constraint
+        [MinLength(10)]  
+        [MaxLength(60)]
+        public string Email { get; set; }
+        
+        [MaxLength(60)]
+        public string Username { get; set; }
+        
+        [MaxLength(60)]
+        public string FullName { get; set; }
+
+        public ApplicationUser BaseUser { get; set; } // creates one to one relationship to another table
+
+        public List<Board> Boards { get; set; } // 1 to many (boards) relationship. Navigation property. Does join sql statement for you
+    }
+}
