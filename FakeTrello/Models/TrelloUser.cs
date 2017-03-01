e6c0ccb5be4baef79bb0d87a204d11ed7c220842
Thomas Waters <Thomas.waters05@gmail.com>
2017-02-28 19:14:14 -0800

    adding initial repo with two relationship tables completed

---
+++ FakeTrello/Models/TrelloUser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+//ENTITY is our Object Relational Mapper (ORM) - So it knows how to interpret this information
+
+namespace FakeTrello.Models
+{
+    public class TrelloUser
+    {
+        [Key] //this a data annotation - this tells the system who/where the primary key is (w/e is right below it)
+        public int TrelloUserId { get; set; } //**THIS IS OUR PRIMARY KEY**
+
+        [MinLength(10)] //you have the ability to stack these properties
+        [MaxLength(60)]
+        public string Email { get; set; }
+
+        [MaxLength(60)]
+        public string UserName { get; set; }
+
+        [MaxLength(60)]
+        public string FullName { get; set; }
+
+    }
+}
