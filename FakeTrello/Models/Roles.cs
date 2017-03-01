0ae5035375a1348545897731c9b9138e7b26290d
Alicia <akb.miceli@gmail.com>
2017-02-28 21:32:50 -0600

    all relationships fleshed out based on ERD

---
+++ FakeTrello/Models/Roles.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FakeTrello.Models
+{
+    public class Roles
+    {
+        [Key]
+        public int RoleId { get; set; }
+
+        public string Name { get; set; }
+
+        public List<Permissions> Permissions { get; set; }
+
+        public List<Board> Board { get; set; }
+
+    }
+}
