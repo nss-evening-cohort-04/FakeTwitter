c5403efc2fcfa68e4bb4809f4921b355d86fbf1c
Debra Gordon <debra.m.gordon@gmail.com>
2017-02-28 19:27:22 -0800

    updated the model classes in the project to map them to the ERD we created

---
+++ FakeTrello/Models/Board.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FakeTrello.Models
+{
+    public class Board
+    {
+        [Key]
+        public int BoardId { get; set; } //Primary key
+
+        public string Name { get; set; } //Board Name
+
+        public string URL { get; set; } //Board URL
+
+        public TrelloUser User { get; set; } // 1 to 1 relationship
+
+        public List<List> Lists { get; set; } // 1 to many (lists) relationship
+
+    }
+}
---
+++ FakeTrello/Models/Card.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FakeTrello.Models
+{
+    public class Card
+    {
+        [Key]
+        public int CardId { get; set; } //Primary key
+
+        public string Name { get; set; } //Card Name
+
+        public string Description { get; set; } //Card Description
+
+        public List List { get; set; } // 1 to 1 relationship
+
+    }
+}
