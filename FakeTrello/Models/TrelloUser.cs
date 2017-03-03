b0f2b7b48459aaf6674b49f0476402491a475173
Josh Kilgore <kilgore.josh@gmail.com>
2017-03-03 13:39:46 -0600

    add first classes to reference database objects

---
+++ FakeTrello/Models/TrelloUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+//Entity is our Object Relational Mapper (ORM)
+
+namespace FakeTrello.Models
+{
+    public class TrelloUser
+    {
+        [Key]
+        public int TrelloUserId { get; set; } //Primary Key
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string FullName { get; set; }
+    }
+}
