698dead02952d4b352220bf89edb4040c4d2567c
Josh Kilgore <kilgore.josh@gmail.com>
2017-03-04 10:57:10 -0600

    add new properties on classes

--- FakeTrello/Models/Card.cs
+++ FakeTrello/Models/Card.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,11 @@ namespace FakeTrello.Models
 {
     public class Card
     {
+        [Key]
+        public int CardId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
     }
 }
