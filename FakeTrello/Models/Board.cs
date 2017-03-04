900700c03e4827607f48e4653a442e5160e0f583
Taylor Harry <taylor.andrew.harry@gmail.com>
2017-03-04 09:55:21 -0800

    completed modeling from ERD

---
+++ FakeTrello/Models/Board.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FakeTrello.Models
+{
+    public class Board
+    {
+        [Key]
+        public int BoardId { get; set; }
+
+        public string Name { get; set; }
+
+        public string URL { get; set; }
+
+        //Auxillary (i.e. not required by drive/define relationship, 
+        //but good practice to go ahead and implement it)
+        public TrelloUser Owner { get; set; }
+
+        public List<List> Lists { get; set; }
+
+    }
+}
---
+++ FakeTrello/Models/Contributer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FakeTrello.Models
+{
+    public class Contributer
+    {
+        [Key]
+        public int TrelloUserId { get; set; }
+
+        public int CardId { get; set; }
+    }
+}
