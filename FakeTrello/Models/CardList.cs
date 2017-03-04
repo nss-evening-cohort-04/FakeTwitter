bb40206a0f53895e6732e6ac9f2003d930ecb824
Alex Saber <alexsaber89@gmail.com>
2017-03-03 22:03:49 -0600

    implement table classes

---
+++ FakeTrello/Models/CardList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FakeTrello.Models
+{
+    public class CardList
+    {
+        public int CardId { get; set; }
+
+        public int ListId { get; set; }
+    }
+}
