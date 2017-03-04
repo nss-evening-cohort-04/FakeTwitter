3f79861edd98761d218d249e63f8006049cd7fa7
Justin Leggett <justin@codingrookie.com>
2017-03-04 10:56:44 -0600

    completed database schema in project

---
+++ FakeTrello/Models/Board.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FakeTrello.Models
+{
+    public class Board
+    {
+        [Key]
+        public int BoardId { get; set; }
+
+        public string Name { get; set; }
+
+        public string URL { get; set; }
+
+        // Auxiliary (i.e. not required to drive/define relationship)
+        public TrelloUser Owner { get; set; }
+
+        public List<List> Lists { get; set; }
+    }
+}
