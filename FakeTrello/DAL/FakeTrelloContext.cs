8973ea674a3524fbe9784c472736cbe31c637722
Josh Kilgore <kilgore.josh@gmail.com>
2017-03-06 17:31:26 -0600

    add classes per ERD

--- FakeTrello/DAL/FakeTrelloContext.cs
+++ FakeTrello/DAL/FakeTrelloContext.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using FakeTrello.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace FakeTrello.DAL
 {
-    public class FakeTrelloContext
+    public class FakeTrelloContext : ApplicationDbContext
     {
     }
 }
