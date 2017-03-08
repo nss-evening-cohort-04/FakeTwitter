4e5b16302dca12f0494b6bafffea59cbadf4c225
Alex Saber <alexsaber89@gmail.com>
2017-03-07 18:42:57 -0600

    notes

---
+++ FakeTrello/ReverseModels/AspNetUsers.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FakeTrello.ReverseModels
+{
+    public class AspNetUsers
+    {
+        [Key]
+        [MaxLength(128)]
+        public string Id { get; set; }
+
+        public string Email { get; set; }
+
+        [Required]
+        public bool EmailConfirmed { get; set; }
+
+        public string PasswordHash { get; set; }
+
+        public string SecurityStamp { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        public bool PhoneNumberConfirmed { get; set; }
+
+        public bool TwoFactorEnabled { get; set; }
+
+        public DateTime LockoutEndDateUtc { get; set; }
+
+        public bool LockoutEnabled { get; set; }
+
+        public int AccessFailedCount { get; set; }
+
+        public string UserName { get; set; }
+
+        public ICollection<AspNetRoles> Roles { get; set; }
+    }
+}
