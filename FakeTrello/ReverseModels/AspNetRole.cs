15e0c01a24e602434c1eb1a78e7ae5b31e4c977a
Alicia <akb.miceli@gmail.com>
2017-03-11 13:05:40 -0600

    changes

---
+++ FakeTrello/ReverseModels/AspNetRole.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FakeTrello.ReverseModels
+{
+    public class AspNetRole
+    {
+        [Key]
+        [MaxLength(128)]
+        public string Id { get; set; }
+
+        [MaxLength(256)]
+        public string Name { get; set; }
+
+        public ICollection<AspNetUser> Users { get; set; }
+    }
+}
