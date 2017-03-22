980b508cfb8606992a362e5c862af328776406ff
Nathan Gonzalez <ncgonz@gmail.com>
2017-03-21 20:06:27 -0500

    oops, forgot to save this file

--- FakeTrello/DAL/Repository/ListRepository.cs
+++ FakeTrello/DAL/Repository/ListRepository.cs
@@ -1,7 +1,35 @@
-﻿namespace FakeTrello.DAL
+﻿using System;
+using System.Collections.Generic;
+//using FakeTrello.Controllers.Contracts;
+using FakeTrello.Models;
+
+namespace FakeTrello.DAL.Repository
 {
-    public class ListRepository
+    public class ListRepository : IListRepository
     {
-        
+        public void AddList(string name, Board board)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddList(string name, int boardId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List GetList(int listId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<List> GetListsFromBoard(int boardId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RemoveList(int listId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
